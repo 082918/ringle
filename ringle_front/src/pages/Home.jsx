f613eb0e17088229c39c27047329435303cc2a8e
가을 <ssruyj510@gmail.com>
2025-05-26 10:20:32 +0900

    Nav, Header, Hom.jsx

--- ringle_front/src/pages/Home.jsx
+++ ringle_front/src/pages/Home.jsx
@@ -1,34 +1,19 @@
-import "./Home.css";
 import { Container, Row, Col } from "react-bootstrap";
 import pro1 from "../assets/pro1.png";
+import "./Home.css";
 
 function Home() {
   return (
-    <div className="homeContent">
-      <Container fluid>
-        {/* ㄴ fluid : 모든 뷰포트 및 기기 크기에서 width : 100% 사용 가능 */}
+    <div>
+      <Container>
         <Row>
-          <Col lg={3} md={4} sm={6} xs={12} className="content">
-            <div className="contentBox">
-              <img src={pro1} className="contentImg" />
-              <div className="contentName">체이싱래빗 퍼퓸 핸드크림 50ml</div>
-              <div className="price"> 9,900 원</div>
-            </div>
+          <Col sm={8} style={{ border: "1px solid black" }}>
+            1 of 3
           </Col>
-          <Col lg={3} md={4} sm={6} xs={12} className="content">
-            <div className="contentBox">
-              <img src="/images/shoes2.png" className="contentImg" />
-              <div className="contentName">Red Knit</div>
-              <div className="price">10,800 원</div>
-            </div>
+          <Col sm={4} style={{ border: "1px solid black" }}>
+            2 of 3
           </Col>
-          <Col lg={3} md={4} sm={6} xs={12} className="content">
-            <div className="contentBox">
-              <img src="/images/shoes3.png" className="contentImg" />
-              <div className="contentName"> 웅냥냥핸드크림 </div>
-              <div className="price"> 7,500 원 ~ </div>
-            </div>
-          </Col>
+          <Col style={{ border: "1px solid black" }}>3 of 3</Col>
         </Row>
       </Container>
     </div>
