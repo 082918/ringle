88357db5b9beed1026fce502b3bf40f239d42fa3
가을 <ssruyj510@gmail.com>
2025-06-05 17:40:56 +0900

    event, header 수정

--- ringle_front/src/layout/Header.jsx
+++ ringle_front/src/layout/Header.jsx
@@ -1,4 +1,5 @@
 import Carousel from "react-bootstrap/Carousel";
+import babo from "../babo.png";
 import "./Header.css";
 
 function Header() {
@@ -6,24 +7,21 @@ function Header() {
     <>
       <Carousel fade>
         <Carousel.Item interval={8000}>
-          {/* <ExampleCarouselImage text="First slide" /> */}
           <div className="hdContatiner"> </div>
           <Carousel.Caption>
-            <h3>First slide label</h3>
+            <h3 className="captionText"> </h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          {/* <ExampleCarouselImage text="Second slide" /> */}
           <div className="hdContatiner"> </div>
           <Carousel.Caption>
-            <h3>Second slide label</h3>
+            <h3 className="captionTex">Second slide label</h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          {/* <ExampleCarouselImage text="Third slide" /> */}
           <div className="hdContatiner"> </div>
           <Carousel.Caption>
-            <h3>Third slide label</h3>
+            <h3 className="captionTex">Third slide label</h3>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
--- ringle_front/src/pages/Event.jsx
+++ ringle_front/src/pages/Event.jsx
@@ -1,48 +1,13 @@
 import data from "../data.jsx";
-import "./Best.css";
-import { useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
 
-function Best() {
-  let [product] = useState(data);
+function Event() {
   return (
     <>
-      <div className="BestTop">
-        <div>
-          <h2> 랭킹 </h2>
-          <p style={{ marginBottom: "8px" }}>
-            이번 주 랭킹! 최근 가장 핫한 상품
-          </p>
-        </div>
+      <div className="EventContainer">
+        <div className="EvetnBox"> 12</div>
       </div>
-      <div className="introContainer">
-        <div className="intro"> 이번주 BEST 상품 </div>
-        <hr style={{ width: "500px" }} />
-      </div>
-      <Container
-        className="justify-content-center"
-        style={{ textAlign: "center", marginTop: "50px" }}
-      >
-        <Row className="justify-content-center">
-          {product.map((product, index) => (
-            <Col md="6" className="contentBox" key={index}>
-              <img
-                src={`/images/${product.img}`}
-                className="contentImg"
-                alt={product.item}
-              />
-              <div className="rankCompany">
-                <div className="rank"> {index + 1}위 </div>
-                <div className="company">{product.company}</div>
-              </div>
-              <div className="item">{product.item}</div>
-              <div className="price">{product.price}</div>
-            </Col>
-          ))}
-        </Row>
-      </Container>
     </>
   );
 }
 
-export default Best;
+export default Event;
