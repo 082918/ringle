c6d0ffbb5d3e78206e02da5f115d899565c744c0
가을 <ssruyj510@gmail.com>
2025-07-04 14:27:13 +0900

    Header, Home 수정정

--- ringle_front/src/pages/Home.jsx
+++ ringle_front/src/pages/Home.jsx
@@ -12,30 +12,32 @@ function Home() {
   return (
     <>
       <Header />
-      <div className="introContainer">
-        <div className="intro"> 이런 제품 어때요? </div>
-        <hr style={{ width: "500px" }} />
+      <div className="homeBody">
+        <div className="introContainer">
+          <div className="intro"> 이런 제품 어때요? </div>
+          <hr style={{ width: "500px" }} />
+        </div>
+        {/* <Link to="/detail"> 홈 </Link> */}
+        <Container
+          className="justify-content-center"
+          style={{ textAlign: "center", marginTop: "50px" }}
+        >
+          <Row className="justify-content-center">
+            {product.map((product, index) => (
+              <Col md="6" className="contentBox" key={index}>
+                <img
+                  src={`/images/${product.img}`}
+                  className="contentImg"
+                  alt={product.item}
+                />
+                <div className="company">{product.company}</div>
+                <div className="item">{product.item}</div>
+                <div className="price">{product.price}</div>
+              </Col>
+            ))}
+          </Row>
+        </Container>
       </div>
-      {/* <Link to="/detail"> 홈 </Link> */}
-      <Container
-        className="justify-content-center"
-        style={{ textAlign: "center", marginTop: "50px" }}
-      >
-        <Row className="justify-content-center">
-          {product.map((product, index) => (
-            <Col md="6" className="contentBox" key={index}>
-              <img
-                src={`/images/${product.img}`}
-                className="contentImg"
-                alt={product.item}
-              />
-              <div className="company">{product.company}</div>
-              <div className="item">{product.item}</div>
-              <div className="price">{product.price}</div>
-            </Col>
-          ))}
-        </Row>
-      </Container>
     </>
   );
 }
